0a3e55ed6092b9ff8e74f5db590fc27ec3169a23
luka8094 <luka8094@stud.kea.dk>
2022-09-22 20:34:05 +0200

    init: setting up initial frontend folder structure and template.
    feature: setting up initial database.
    
    feature: set up accounts router.
    
    test: tested response from router end-point.

---
+++ server/model/account.mjs
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const accountsSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+const Account = mongoose.model("accounts", accountsSchema)
+export default Account
--- server/routers/accountsRouter.mjs
+++ server/routers/accountsRouter.mjs
@@ -1,8 +1,22 @@
+import "dotenv/config"
 import {Router} from "express"
+import Account from "../model/account.mjs"
 const accountsRouter = Router()
 
-accountsRouter.get("/api/test", (req, res) => {
+accountsRouter.get("/api/user", (req, res) => {
     res.send("the server is connected.")
+})
+
+accountsRouter.post("/api/login", (req, res) => {
+    console.log("login endpoint")
+})
+
+accountsRouter.post("/api/register", (req, res) => {
+    console.log("register endpoint")
+})  
+
+accountsRouter.post("/api/logout", (req, res) => {
+    console.log("logout endpoint")
 })
 
 export default accountsRouter
