88b41359bcca7f153d783ff3566c186f68f1eec2
luka8094 <luka8094@stud.kea.dk>
2022-09-23 19:56:03 +0200

    feature: setting up route components.

---
+++ client/knox/src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<section>
+    Footer
+</section>
+
+<style>
+    section{
+        display: flex;
+        height: 100vh;
+        width: 100%;
+        background: rgba(10,10,10,.5);
+    }
+</style>
--- client/knox/src/pages/Chatrooms.svelte
+++ client/knox/src/pages/Chatrooms.svelte
@@ -8,5 +8,7 @@
         height: 100vh;
         width: 100%;
         background: rgba(100,50,100,.5);
+        align-items: center;
+        justify-content: center;
     }
 </style>
