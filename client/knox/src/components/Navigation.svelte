3607188701a2bea5085e6a87594aaa101ecb937c
luka8094 <luka8094@stud.kea.dk>
2022-09-24 17:45:05 +0200

    style: added a logo for the menu navigation. Still WIP.

--- client/knox/src/components/Navigation.svelte
+++ client/knox/src/components/Navigation.svelte
@@ -15,6 +15,7 @@
 
     let navigationBar, logo, logoName, logoImage, menuOverlay
     let menuMain, menuMarket, menuLogin, menuRegister, menuChatrooms
+
     let screenWidth = window.innerWidth / 2
     let screenHeight = window.innerHeight / 2
 
@@ -31,29 +32,28 @@
             .set(menuLogin, {top: "-100%"})
             .set(menuRegister, {top: "-100%"})
             .set(menuChatrooms, {top: "-100%"})
+            .set(logoImage, {height: "3.5em", width: "3.5em"})
             .set(logo, {display: "none", width: 0, alpha: "0", x: screenWidth , y: screenHeight, xPercent:"-50", yPercent:"-50", zIndex: 10}, "+=1")
-            .set(logoName, {display: "none", width: 0, fontSize: "2em", opacity: 0}, "+=1")
-            .to(logo, {display: "flex", duration: 2, width: 100, opacity: 1, x: screenWidth , y: screenHeight, xPercent:"-100", yPercent:"-50"}, "+=1")
+            .set(logoName, {display: "none", width: 0, fontSize: "1.8em", opacity: 0}, "+=1")
+            .to(logo, {display: "flex", duration: 2, width: 150, height: 75, opacity: 1, x: screenWidth , y: screenHeight, xPercent:"-100", yPercent:"-50"}, "+=1")
             .delay(1)
-            .to(logoImage, {duration, marginRight: "auto"}, "+=1")
+            .to(logoImage, {duration, height: "45px", width: "45px", marginRight: "auto"}, "+=1")
             .to(logo, {duration, width: "200px"}, "+=1")
-            .to(logoName, {duration: 1.5, display: "flex", width: "100%", alpha: "1", ease: "power2.easeIn"}, "+=1")
+            .to(logoName, {duration: 1.5, display: "flex", width: "100%", lineHeight: "15px", alpha: "1", ease: "power2.easeIn"}, "+=1")
             .set(logo, {duration, opacity: 1, x: screenWidth , y: screenHeight}, "+=1")
             .delay(1)
-            .fromTo(logo, {opacity: 1, fontSize: "2em", x: screenWidth , y: screenHeight}, {duration: 2, fontSize: "8px", x: 0 , y:0, xPercent:"0", yPercent:"0"}, "+=1")
+            .fromTo(logo, {opacity: 1, fontSize: "2em", x: screenWidth , y: screenHeight}, {duration: 2, height: 60, fontSize: "8px", x: 0 , y:0, xPercent:"0", yPercent:"0"}, "+=1")
             .delay(1)
             .add('overlay')
             .to(menuOverlay, {duration: .5, opacity: 0}, "+=1", 'overlay')
             .to(menuOverlay, {display: "none", zIndex: "-10"}, "+=1", 'overlay')
             .to(navigationBar, {duration, position: "fixed", boxShadow: "1px 1px 5px 1px"}, "+=1.2", 'overlay')
             .add('menu-rollout', 5)
-            .to(menuMain, {duration: 1, top: "0%", ease: "power2.easeOut", delay: 1}, "-=.98", 'menu-rollout')
+            .to(menuMain, {duration: 1, top: "0%", ease: "power2.easeOut", delay: 1}, "-=.970", 'menu-rollout')
             .to(menuMarket, {duration: 1, top: "0%", ease: "power2.easeOut", delay: .9}, "-=.975", 'menu-rollout')
             .to(menuLogin, {duration: 1, top: "0%", ease: "power2.easeOut", delay: .7}, "-=.95", 'menu-rollout')
             .to(menuRegister, {duration: 1, top: "0%", ease: "power2.easeOut", delay: .5}, "-=.925", 'menu-rollout')
-            .to(menuChatrooms, {duration: 1, top: "0%", ease: "power2.easeOut", delay: .3},"-=.9", 'menu-rollout')
-
-            
+            .to(menuChatrooms, {duration: 1, top: "0%", ease: "power2.easeOut", delay: .3},"-=.9", 'menu-rollout')   
         }
         return () => { 
             visited.set(true)
@@ -67,7 +67,7 @@
         <div bind:this={logo} class="logo">
             <a href="/">
                 <picture bind:this={logoImage}>
-
+                    <img src="images/knox-logo.svg.svg" alt="knox logo"/>
                 </picture>
             </a>
             <p bind:this={logoName}>KNOX</p>
@@ -111,7 +111,7 @@
 <style>
     nav{
         display: flex;
-        height: 50px;
+        height: 60px;
         width: 100%;
         margin-left: auto;
         align-items: center;
@@ -140,14 +140,16 @@
     p{
         display: none;
         opacity: 0;
+        color: black;
+        align-self: center;;
     }
 
     picture{
         display: flex;
-        height: 40px;
-        width: 40px;
+        height: 45px;
+        width: 45px;
         margin: 0 10px;
-        background: rgba(50,100,10,.5);
+        background: transparent;
         border-radius: 50px;
     }
 
