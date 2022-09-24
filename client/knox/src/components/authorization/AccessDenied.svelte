23ad614b46ef6c9ffa685e310ea78a803281c71b
luka8094 <luka8094@stud.kea.dk>
2022-09-24 09:15:38 +0200

    feature: added component template.

---
+++ client/knox/src/components/authorization/AccessDenied.svelte
@@ -0,0 +1,12 @@
+<section>
+    Access denied
+</section>
+
+<style>
+    section{
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(100,10,150,.5);
+    }
+</style>
