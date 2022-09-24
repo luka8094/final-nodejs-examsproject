b5461b81beb3342b374f5b917d9f2c793234fd0c
luka8094 <luka8094@stud.kea.dk>
2022-09-24 14:18:58 +0200

    feature: added overlay animation effect to landing page.
    refactor: refactored navigation bar elements structure.
    
    style: added preliminary styling to the navigation bar elements.

--- client/knox/src/components/Navigation.svelte
+++ client/knox/src/components/Navigation.svelte
@@ -1,6 +1,6 @@
 <script>
     import gsap from "gsap/dist/gsap"
-    import {onMount} from "svelte"
+    import {onMount, onDestroy} from "svelte"
     import {Router, Link, Route} from 'svelte-navigator'
     import PrivateRoute from './authorization/PrivateRoute.svelte'
     import {user, visited} from "../../stores/systemd"
@@ -11,42 +11,52 @@
     import Register from '../pages/Register.svelte'
     import Market from '../pages/Market.svelte'
     import AccessDenied from './authorization/AccessDenied.svelte'
+    import LoginForm from "./account/LoginForm.svelte";
 
-    let logo, logoName
+    let navigationBar, logo, logoName, logoImage, menuOverlay
     let screenWidth = window.innerWidth / 2
     let screenHeight = window.innerHeight / 2
 
-    $: if(!$visited){
-        onMount(() => {
+    onMount(() => {     
+        if(!$visited){
             const timeline = gsap.timeline()
             const duration = 1
             timeline
-            .set(logo, {display: "none", width: 0, opacity: 0, x: screenWidth , y: screenHeight, xPercent:"-150", yPercent:"-50"}, "+=1")
-            .set(logoName, {display: "none", opacity: 0}, "+=1")
+            .set(navigationBar, {position:"absolute"}, "+=1")
+            .set(logo, {display: "none", width: 0, alpha: "0", x: screenWidth , y: screenHeight, xPercent:"-150", yPercent:"-50", zIndex: 10}, "+=1")
+            .set(logoName, {display: "none", width: 0, fontSize: "2em", opacity: 0}, "+=1")
             .to(logo, {display: "flex", duration: 2, width: 100, opacity: 1, x: screenWidth , y: screenHeight, xPercent:"-150", yPercent:"-50"}, "+=1")
             .delay(1)
-            .set(logoName, {display: "flex", opacity: 1}, "+=1")
-            .delay(3)
+            .to(logoImage, {duration, marginRight: "auto"}, "+=1")
+            .to(logo, {duration, width: "200px"}, "+=1")
+            .to(logoName, {duration: 1.5, display: "flex", width: "100%", alpha: "1"}, "+=1")
             .set(logo, {duration, opacity: 1, x: screenWidth , y: screenHeight}, "+=1")
             .delay(1)
-            .set(logoName, {display: "flex", opacity: 1}, "+=1")
-            .fromTo(logo, {opacity: 1, x: screenWidth , y: screenHeight}, {duration: 2, x: 0 , y:0, xPercent:"0", yPercent:"0"}, "+=1")
-        })
-    }else{ 
-        $visited = true
-        logo.style.opacity = "1;"
-    }
+            .fromTo(logo, {opacity: 1, fontSize: "2em", x: screenWidth , y: screenHeight}, {duration: 2, fontSize: "8px", x: 0 , y:0, xPercent:"0", yPercent:"0"}, "+=1")
+            .delay(1)
+            .add('overlay')
+            .to(menuOverlay, {duration, opacity: 0}, "+=1", 'overlay')
+            .to(menuOverlay, {display: "none", zIndex: "-10"}, "+=1", 'overlay')
+            .to(navigationBar, {position: "fixed"}, "+=1", 'overlay')
+        }
+        return () => { 
+            visited.set(true)
+        }
+    })
+    
 </script>
 
 <Router>
-    <nav>
+    <nav bind:this={navigationBar}>
         <div bind:this={logo} class="logo">
-            <picture>
+            <a href="/">
+                <picture bind:this={logoImage}>
 
-            </picture>
+                </picture>
+            </a>
             <p bind:this={logoName}>KNOX</p>
         </div>
-        <div>
+        <div id="menu-container">
             <Link to="/">Main</Link>
             <Link to="market">Market</Link>
             {#if !$user}
@@ -58,6 +68,9 @@
             <Link to="chatrooms">Chatrooms</Link>
         </div>
     </nav>
+    <div bind:this={menuOverlay} id="menu-overlay">
+            
+    </div>
     <Route path="/" component={Main}/>    
     <Route path="market" component={Market}/>
     <Route path="login" component={Login}/>
@@ -72,35 +85,47 @@
 <style>
     nav{
         display: flex;
-        height: 150px;
+        height: 50px;
         width: 100%;
         margin-left: auto;
         align-items: center;
+        background: white;
+    }
+
+    a{
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        height: 100%;
+        margin: 10px;
     }
 
     .logo{
-        display: flex;
+        display: none;
         position: absolute;
         height: 50px;
         width: 120px;
         margin-left: 100px;
         background: rgba(5,100,50,.5);
         opacity: 0;
+        align-items: center;
     }
 
     p{
-        display: flex;
+        display: none;
+        opacity: 0;
     }
 
     picture{
         display: flex;
         height: 40px;
         width: 40px;
+        margin: 0 10px;
         background: rgba(50,100,10,.5);
         border-radius: 50px;
     }
 
-    div{
+    #menu-container{
         display: flex;
         height: 100%;
         width: 50%;
@@ -108,5 +133,13 @@
         background: rgba(100,50,10,.5);
         align-items: center;
         justify-content: space-evenly;
+    }
+
+    #menu-overlay{
+        display: flex;
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        background: white;
     }
 </style>
