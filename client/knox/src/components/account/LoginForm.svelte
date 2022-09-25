fc3153235ff34b9e4d31bb93f67aa6627dc45875
luka8094 <luka8094@stud.kea.dk>
2022-09-25 08:44:00 +0200

    style: changed height on login form.
    style: added a third revised version of the logo in the images folder.

--- client/knox/src/components/account/LoginForm.svelte
+++ client/knox/src/components/account/LoginForm.svelte
@@ -10,19 +10,33 @@
 
 <form on:submit={login}>
     <label for="userEmail">Email:</label>
-    <input type="email" name="userEmail">
-    <label for="userPassword">Password:</label>
-    <input type="password" name="userPassword">
-    <button type="submit">login</button>
+    <input type="email" name="userEmail" placeholder="type your email"> 
+    <label for="userpassword">Password:</label>
+    <input type="password" name="userpassword" placeholder="pick a nice password">
+    <button type="submit">register</button>
 </form>
 
 <style>
     form{
         display: flex;
         flex-direction: column;
-        height: 100px;
-        width: 200px;
+        height: 250px;
+        width: 370px;
         align-items: center;
+        padding: 10px;
         justify-content: center;
+        border: dashed 1px black;
+    }
+
+    label{
+        margin: 5px 0;
+    }
+
+    form *{
+        width: 100%;
+    }
+
+    button{
+        margin-top: auto;
     }
 </style>
