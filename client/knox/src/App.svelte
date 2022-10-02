3f2f1f84f7bc6d5181df15a64e6dcd16e79fb0fe
luka8094 <luka8094@stud.kea.dk>
2022-10-02 20:09:43 +0200

    init: set up OpenZeppelin smart contracts.

--- client/knox/src/App.svelte
+++ client/knox/src/App.svelte
@@ -1,12 +1,19 @@
 <script>
+	import {getCookie} from "svelte-cookie"
 	import Navigation from "./components/Navigation.svelte"
 	import ToastContainer from "./modules/ToastContainer.svelte"
 	import CookieBanner from "./components/CookieBanner.svelte"
+
+	$: cookieConsent = getCookie('_kc')
 </script>
 
+{@debug cookieConsent}
 <Navigation />
 <ToastContainer />
-<CookieBanner />
+
+{#if cookieConsent === null}
+	<CookieBanner />
+{/if}
 
 <style>
 	:root::-webkit-scrollbar{
